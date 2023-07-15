6c279953338c2b8a7340dd5ea2d7b74c4caea76c
ShahRiarKhan55 <siaabid1234@gmail.com>
2023-07-16 01:48:23 +0600

    login created

---
+++ uiucanteey/src/auth/FacultyLogin.js
@@ -0,0 +1,87 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Button,
+  Heading,
+  useColorModeValue,
+  Divider,
+  Center,
+} from "@chakra-ui/react";
+
+export default function FacultyLogin(props) {
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("#faeee7")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textColor={"#33272a"}>
+            Sign in to your account
+          </Heading>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <Heading
+              fontSize={"4xl"}
+              textAlign={"center"}
+              textColor={"#33272a"}
+            >
+              Faculty
+            </Heading>
+            <FormControl id="email">
+              <FormLabel>Email</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              ></Stack>
+              <Button
+                bg={"#ff8ba7"}
+                color={"#33272a"}
+                _hover={{
+                  bg: "#c3f0ca",
+                }}
+              >
+                Sign in
+              </Button>
+            </Stack>
+            <Divider />
+            <Center>
+              <Button
+                w="100%"
+                maxW={"md"}
+                variant={"outline"}
+                bg={"#c3f0ca"}
+                color={"#33272a"}
+                _hover={{
+                  bg: "#ff8ba7",
+                }}
+              >
+                Sign Up
+              </Button>
+            </Center>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
