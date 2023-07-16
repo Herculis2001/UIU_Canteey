5fc6a0e232604d6d37804752e37a1020c2416a10
ShahRiarKhan55 <siaabid1234@gmail.com>
2023-07-16 16:59:44 +0600

    2nd commit to this branch

--- uiucanteey/src/App.tsx
+++ uiucanteey/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import StudentLogin from "./auth/StudentLogin";
+import FacultyLogin from "./auth/FacultyLogin";
+import OwnerLogin from "./auth/OwnerLogin";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/studentlogin" element={<StudentLogin />} />
+          <Route path="/facultylogin" element={<FacultyLogin />} />
+          <Route path="/ownerlogin" element={<OwnerLogin />} />
+          {/* <Route
+                path="/userlogin"
+                element={
+                  <UserLogin
+                    loginSTUHandle={loginSTUhandle}
+                    STUrole={STUrole}
+                  />
+                }
+              /> */}
+          {/* <Route path="/register" element={<Register />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
